c0ef4239cba39ffe49d5de76dbd8ba0d10cda80b
unknown <shikderhafiz@gmail.com>
2018-11-28 17:39:16 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Game extends Component {
+  render ( ){
+    return (
+      <div className="style">
+      <div>Put components here</div>
+      <div>Who won??</div>
+      <button type="button">Play the Game</button>
+      </div>
+    )
+  }
+}
---
+++ src/PlayerCard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PlayerCard = (props) => {
+  return(
+    <div className="playercard"</div>
+  )
+}
